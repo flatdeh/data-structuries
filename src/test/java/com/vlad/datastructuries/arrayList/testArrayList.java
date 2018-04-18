c2cb7404d980fcec9c67d1cb0d70d4006facdf18
Vladislav Degtiar <flatdeh@gmail.com>
2018-04-18 22:36:08 +0300

    Stack&Queue

--- src/test/java/com/vlad/datastructuries/arrayList/testArrayList.java
+++ src/test/java/com/vlad/datastructuries/arrayList/testArrayList.java
@@ -1,4 +1,46 @@
 package com.vlad.datastructuries.arrayList;
 
+import org.junit.Test;
+
 public class testArrayList {
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testRemove() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testSet() {
+    }
+
+    @Test
+    public void testClear() {
+    }
+
+    @Test
+    public void testSize() {
+    }
+
+    @Test
+    public void testIsEmpty() {
+    }
+
+    @Test
+    public void testContains() {
+    }
+
+    @Test
+    public void testIndexOf() {
+    }
+
+    @Test
+    public void testLastIndexOf() {
+    }
+    
 }
